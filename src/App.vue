a913372b1feb02b08d600f0de6ad5921de50a7f7
sergiodario <sergiochocobar2@gmail.com>
2022-10-24 21:47:10 -0300

    agregando iconos de engranaje y mejorando el navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
-    <!-- <router-link to="/">Home</router-link>
-    <router-link to="/user">User</router-link> -->
-    
+
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container">
-        <router-link class="navbar-brand" to="/">PharmApp</router-link>
+        <router-link class="navbar-brand" to="/"><img src="./assets/logoPharmApp.png" style="width: 55px"/>PharmApp</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
+            <li class="nav-item pe-3">
+              <router-link  class="nav-link" to="/"><img src="./assets/user-solid.png" style="width: 25px"/></router-link>
             </li>
             <li class="nav-item">
-              <router-link  class="nav-link" to="/user"><img class="iconoBoton" src="./assets/user-solid.png" style="width: 20px"/></router-link>
+              <router-link  class="nav-link" to="/user"><img src="./assets/gear-solid.png" style="width: 25px"/></router-link>
             </li>
           </ul>
         </div>
